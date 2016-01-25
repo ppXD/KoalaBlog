54d797885b028d0d909b6655f7d9e091253d223c
ppXD <459406671@qq.com>
2016-01-25 19:53:41 +0800

    initial draft version

--- KoalaBlog.Web/obj/Release/Package/PackageTmp/Content/Scripts/Services/PersonService.js
+++
@@ -1,7 +0,0 @@
-﻿app.factory("PersonService", ["AjaxService", function (AjaxService) {
-    return {
-        getPersonInfo: function (successFunction, errorFunction, failedFunction) {
-            AjaxService.AjaxGet("../Person/GetPersonInfo", successFunction, errorFunction, failedFunction);
-        }
-    }
-}]);
